039166fc93b9ba0541e65485b3b8693e818c24ef
senpudev <senpudev@gmail.com>
2025-12-11 15:53:19 +0100

    update imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import { FiltersProvider } from './context/filters.jsx'
-import { ScrollRefsProvider } from './context/scrollRefs.jsx'
-import { LanguageProvider } from './context/language.jsx'
-import { ModalProvider } from './context/modal.jsx'
+import { FiltersProvider } from './context/Filters.jsx'
+import { ScrollRefsProvider } from './context/ScrollRefs.jsx'
+import { LanguageProvider } from './context/Language.jsx'
+import { ModalProvider } from './context/Modal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
